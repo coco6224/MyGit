c8bca2c9b6bb4ac1c10a1cd7203a348190c21a22
RitaKuo-PC\Rita Kuo <coco6224@gmail.com>
2018-04-14 23:12:20 +0800

    no message

---
+++ OShw/ch4_2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<thread>
+#include<vector>
+using namespace std;
+
+vector<int> fib;
+
+void generate(int n){
+  if(fib.size()<n-1){
+    generate(n-1);
+  }else{
+    fib.push_back(fib[n-2]+fib[n-3]);
+  }
+}
+
+int main(){
+  while(true){
+    int num;
+    cin>>num;
+    cout<<endl;
+    generate(num);
+    for(vector<int>::iterator it = fib.begin();it!=fib.end();it++){
+      cout<<*it<<endl;
+    }
+    cout<<endl;
+    fib.clear();
+  }
+}
