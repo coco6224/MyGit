5cd2f9ffc419c097eb3a2689bc3dc2a628a3068e
RitaKuo-PC\Rita Kuo <coco6224@gmail.com>
2018-04-14 22:32:28 +0800

    no message

--- OShw/hw2_1.cpp
+++ OShw/hw2_1.cpp
@@ -6,37 +6,44 @@
 
 using namespace std;
 
-void avrage(vector<int>nums, int &output) {
+double avr;
+double max;
+double min;
+
+void avrage(vector<int>nums) {
 	int temp = 0;
 	for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
 		temp += *it;
 	}
-	output = temp / nums.size();
+	avr = temp / nums.size();
 }
 
-void minimum(vector<int>nums, int &output) {
+void minimum(vector<int>nums) {
 	int temp = 999999999;
 	for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
 		if (*it < temp) {
 			temp = *it;
 		}
 	}
-	output = temp;
+	min = temp;
 }
 
-void maximum(vector<int>nums, int &output) {
+void maximum(vector<int>nums) {
 	int temp = -9999999999;
 	for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
 		if (*it > temp) {
 			temp = *it;
 		}
 	}
-	output = temp;
+	max = temp;
 }
 
 int main()
 {
 	while (true) {
+		avr = -1;
+		min = -1;
+		max = -1;
 		string s;
 		getline(cin, s);
 		stringstream ss;
@@ -47,19 +54,34 @@ int main()
 			nums.push_back(num);
 		}
 		if (nums.size() > 0) {
-			int avr;
-			int max;
-			int min;
 			thread avra(avrage, ref(nums), ref(avr));
 			thread mini(minimum, ref(nums), ref(min));
 			thread maxi(maximum, ref(nums), ref(max));
 			avra.join();
 			mini.join();
 			maxi.join();
-			cout << "The avrage value is " << avr << endl;
-			cout << "The minimum value is " << min << endl;
-			cout << "The maximum value is " << max << endl << endl;
 		}
+		int count = 0;
+		while(count<3){
+			if(avr!=-1){
+				cout << "The avrage value is " << avr << endl;
+				count++;
+				avr = -1;
+			}
+
+			if(min!=-1){
+				cout << "The minimum value is " << min << endl;
+				count++;
+				avr = -1;
+			}
+
+			if(max!=-1){
+				cout << "The maximum value is " << max << endl;
+				count++;
+				avr = -1;
+			}
+		}
+		cout << endl;
 	}
 	return 0;
 }
